1725201764cf062e9eadb41ea2891cdceb6802cb
TarsooPaulo <paulodetarse333@gmail.com>
2024-06-19 16:05:26 -0300

    update settings.py

--- meusite/settings.py
+++ meusite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z@(u47afdiz_sgr2*v-(n5odhbtj%wp4zr+qnd%h)pu-6^&h70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
